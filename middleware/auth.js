f38585563fd243d58bdfdf05fed5c724d2bf0df9
pppgod <pppgod@pppgoddeMacBook-Air.local>
2026-04-18 02:58:30 +0800

    feat: 去掉微信登录逻辑，不需要登录即可使用所有功能

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,7 @@
-const { get, run } = require('../db');
-
 function authMiddleware(req, res, next) {
-  const openid = req.headers['x-wx-openid'];
-
-  if (!openid) {
-    return res.status(401).json({ code: 401, message: '未获取到用户身份' });
-  }
-
-  req.userId = openid;
-
-  // 自动创建用户（如果不存在）
-  get('SELECT * FROM users WHERE openid = ?', [openid])
-    .then(user => {
-      if (!user) {
-        return run('INSERT INTO users (openid) VALUES (?)', [openid]);
-      }
-    })
-    .then(() => next())
-    .catch(err => {
-      console.error('自动创建用户失败:', err);
-      next();
-    });
+  // 不需要登录，默认用测试用户
+  req.userId = 'test_user_001';
+  next();
 }
 
 module.exports = authMiddleware;
--- miniapp/app.js
+++ miniapp/app.js
@@ -1,49 +1,9 @@
 App({
   globalData: {
-    openid: null
+    openid: 'test_user_001'
   },
 
-  async onLaunch() {
-    // 微信登录获取openid
-    await this.login();
-  },
-
-  // 微信登录
-  async login() {
-    return new Promise((resolve, reject) => {
-      wx.login({
-        success: async (res) => {
-          if (res.code) {
-            // 调用登录接口
-            wx.request({
-              url: 'https://schedule-server-247739-4-1423159736.sh.run.tcloudbase.com/auth/login',
-              method: 'POST',
-              data: { code: res.code },
-              success: (result) => {
-                if (result.data.code === 0) {
-                  this.globalData.openid = result.data.openid;
-                  console.log('登录成功，openid:', this.globalData.openid);
-                  resolve(result.data.openid);
-                } else {
-                  console.error('登录接口返回错误:', result.data.message);
-                  reject(result.data.message);
-                }
-              },
-              fail: (err) => {
-                console.error('登录接口请求失败:', err);
-                reject(err);
-              }
-            });
-          } else {
-            console.error('登录失败，没有code:', res.errMsg);
-            reject(res.errMsg);
-          }
-        },
-        fail: (err) => {
-          console.error('wx.login失败:', err);
-          reject(err);
-        }
-      });
-    });
+  onLaunch() {
+    console.log('小程序启动完成，默认用户ID:', this.globalData.openid);
   }
 });
